c8a3edb9d6bf0aded72cab74a074c7150b0ec7f1
Webprojon <tokhirjonkhasanov@gmail.com>
2024-12-03 10:30:26 +0100

    PWA added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: "prompt",
+			includeAssets: ["favicon.ico", "apple-touch-icon.png", "maskable.png"],
+			manifest: {
+				name: "Online Chat",
+				short_name: "Online Chat",
+				description: "React Online Chat App",
+				icons: [
+					{
+						src: "maskable.png",
+						sizes: "196x196",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+					{
+						src: "logo192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "logo512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+				theme_color: "#1E40AF",
+				background_color: "#1E40AF",
+				display: "standalone",
+				scope: "/",
+				start_url: "/",
+				orientation: "portrait",
+			},
+		}),
+	],
+});
